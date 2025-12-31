22215f44c9b1d89a5d04eb68eaf9bb980823fe4b
Nandini <nandinichakaravarthy@gmail.com>
2025-12-31 15:59:52 +0530

    admin base 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome</h1>
-      <p>This is the frontend userflow</p>
+      <p>This is the frontend admin flow</p>
     </div>
   );
 }
